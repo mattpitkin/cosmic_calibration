23f030f0b5a6f68543d10a5fc182c07e8a21dce6
Matthew Pitkin <matthew.pitkin@ligo.org>
2015-07-01 10:35:36 +0100

    Remove whiskers from boxplots

--- src/python/results_script.py
+++ src/python/results_script.py
@@ -182,7 +182,8 @@ for dist in dists:
   positions.append(dist)
   positions.append(dist+10)
  
-bp = pl.boxplot(data, notch=0, sym='+', positions=positions, widths=8, showfliers=False) 
+#bp = pl.boxplot(data, notch=0, sym='+', positions=positions, widths=8)
+bp = pl.boxplot(data, notch=0, sym='', positions=positions, widths=8) 
 
 pl.setp(bp['boxes'], color='black')
 pl.setp(bp['whiskers'], color='black')
