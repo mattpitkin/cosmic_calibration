a5a2bc9d81c6773de97878e25b81dadf4262b31e
Matthew Pitkin <matthew.pitkin@ligo.org>
2022-08-10 12:14:55 +0100

    Remove Liams report

--- liam_src/README.txt
+++
@@ -1,2 +0,0 @@
-These directories contain different versions of the inspiral code for different systems.
-Descriptions of which should be found in each folder.
--- liam_src/random_sky_position/single_detector/closest.m
+++
@@ -1,9 +0,0 @@
-function [val, idx] = closest(a, b)
-
-% [val, idx] = closest(a, b)
-%
-% Find the value in vector b that is closest to the value given in a.
-
-[y, idx] = min(abs(b-a));
-
-val = b(idx);
